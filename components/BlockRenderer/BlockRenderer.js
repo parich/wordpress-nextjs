5b8501cef9e691d7b71f19def51f46fae7b02652
parich suriya <parich.suriya@gmail.com>
2023-10-20 15:30:17 +0700

    Create heading component, util functions, extend tailwind, and inner block

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,8 +1,25 @@
+import { Cover } from "components/Cover";
+import { Heading } from "components/Heading";
+
 export const BlockRenderer = ({ blocks }) => {
   return blocks.map((block) => {
     switch (block.name) {
+      case "core/heading": {
+        return (
+          <Heading
+            key={block.id}
+            level={block.attributes.level}
+            content={block.attributes.content}
+            textAlign={block.attributes.textAlign}
+          />
+        );
+      }
       case "core/cover": {
-        return <div key={block.id}>core cover</div>;
+        return (
+          <Cover key={block.id} background={block.attributes.url}>
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Cover>
+        );
       }
       default:
         return null;
